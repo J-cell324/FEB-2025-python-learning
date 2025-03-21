5c806220f930d3ea424f136a7a1089e1e28a5478
J-cell324 <ndiritujohngithinji@gmail.com>
2025-03-21 18:53:35 +0300

    initial commit

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
 print("hello world")
+
+number = 10
+print(10)
+
+
+
+age = 27   #int data typ
+marks = 90.5  #float
+
+
+
+name ="my name is John Githinji" #string
+#list data types
+languages ={"python", "Html", "css", 3, 5.7, True}
+print(languages)
+print(languages)
